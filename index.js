3b8b7900be9e991794e9a61f626621871604d359
Juan <juanchoto94@gmail.com>
2026-02-02 15:28:47 -0300

    cambio de constante

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require ('express')
+const cors = require ('cors')
+const app = express()
+const PORT = 3000
+
+app.use(cors());
+app.use(express.json());
+
+const catalogo=[
+    {
+        nombre: "Inception",
+        imagen: "https://wallpapercave.com/dwp1x/msi4htu.jpg",
+        genero: "Ciencia Ficcion"
+    }
+]
+
+app.get('/', (req, res) =>{
+    res.json(catalogo);
+})
+
+app.post('/', (req, res) =>{
+    const nuevaPelicula = req.body;
+
+    if (!nuevaPelicula.nombre || !nuevaPelicula.imagen || !nuevaPelicula.genero){
+        return res.status(400).json({ errror: "Faltan datos"});
+    }
+
+    catalogo.push(nuevaPelicula);
+    console.log("Pelicula agregada: ", nuevaPelicula);
+
+    res.status(201).json({
+        mensaje: "Pelicula creada con exito",
+        pelicula: pelicula
+    });
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
