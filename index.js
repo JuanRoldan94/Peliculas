34f864d797c7dea4bd03e832a93e837958d0b83a
Juan <correo.juanroldan@gmail.com>
2026-02-04 15:14:12 -0300

    Fix carga Peliculas

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const PORT = 3000
 app.use(cors());
 app.use(express.json());
 
-const catalogo=[
-    {
-        nombre: "Inception",
-        imagen: "https://wallpapercave.com/dwp1x/msi4htu.jpg",
-        genero: "Ciencia Ficcion"
-    }
-]
+const catalogo=[]
 
 app.get('/', (req, res) =>{
     res.json(catalogo);
@@ -20,10 +14,13 @@ app.get('/', (req, res) =>{
 
 app.post('/', (req, res) =>{
     const nuevaPelicula = req.body;
-
-//    if (!nuevaPelicula.nombre || !nuevaPelicula.imagen || !nuevaPelicula.genero){
-//        return res.status(400).json({ error: "Faltan datos"});
-//    }
+    
+    if (!nuevaPelicula.imagen){
+        nuevaPelicula.imagen = "https://www.shutterstock.com/image-vector/film-reel-vector-cinema-logo-600nw-2353280887.jpg"
+    }
+//  if (!nuevaPelicula.nombre || !nuevaPelicula.imagen || !nuevaPelicula.genero){
+//      return res.status(400).json({ error: "Faltan datos"});
+//  }
 
     catalogo.push(nuevaPelicula);
     console.log("Pelicula agregada: ", nuevaPelicula);
