6e1aa99818ca8cb7d691141a139920684ecbba87
Juan <juanchoto94@gmail.com>
2026-02-08 20:04:26 -0300

    Cambio de estructura

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const express = require ('express')
-const cors = require ('cors')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
 
-app.use(cors());
+app.set('view engine', 'ejs');
+
 app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
 
-const catalogo=[]
+const catalogo = []
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/peliculas', (req, res) => {
     res.json(catalogo);
-})
+});
 
-app.post('/', (req, res) =>{
-    const nuevaPelicula = req.body;
-    
+app.post('/peliculas', (req, res) => {
+    const nuevaPelicula = req.body;;
+
     if (!nuevaPelicula.imagen){
-        nuevaPelicula.imagen = "https://www.shutterstock.com/image-vector/film-reel-vector-cinema-logo-600nw-2353280887.jpg"
+        nuevaPelicula.imagen = "/img/default.jpg";
     }
-//  if (!nuevaPelicula.nombre || !nuevaPelicula.imagen || !nuevaPelicula.genero){
-//      return res.status(400).json({ error: "Faltan datos"});
-//  }
-
+    
     catalogo.push(nuevaPelicula);
-    console.log("Pelicula agregada: ", nuevaPelicula);
+    console.log("Pelicula agregada al catalogo:", nuevaPelicula);
 
-    res.status(201).json({
-        mensaje: "Pelicula creada con exito",
-        pelicula: nuevaPelicula
+    res.status(201).json({ 
+        message: 'Pelicula agregada al catalogo', 
+        pelicula: nuevaPelicula 
     });
 });
 
-app.listen(PORT, () =>{
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+app.listen(PORT, (req, res) => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
