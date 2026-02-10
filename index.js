ac20e831c327b56094cdf335e0c17ab89dd56293
Juan <juanchoto94@gmail.com>
2026-02-10 20:59:22 -0300

    Actualizacion

--- index.js
+++ index.js
@@ -19,8 +19,21 @@ app.get('/peliculas', (req, res) => {
     res.json(catalogo);
 });
 
+app.get('/peliculas/:id', (req, res) => {
+    const id = req.params.id;
+    const pelicula = catalogo.find(p => p.id === id);
+
+    if (pelicula) {
+        res.json(pelicula);
+    } else {
+        res.status(404).json({ message: "Pelicula no encontrada" });
+    }
+});
+
 app.post('/peliculas', (req, res) => {
     const nuevaPelicula = req.body;
+
+    nuevaPelicula.id = Date.now().toString();
 
     if (!nuevaPelicula.imagen){
         nuevaPelicula.imagen = '/img/default.jpg';
@@ -34,6 +47,40 @@ app.post('/peliculas', (req, res) => {
         pelicula: nuevaPelicula 
     });
 });
+
+app.delete('/peliculas/:id', (req, res) => {
+    const idParaBorrar = req.params.id;
+    
+    const indice = catalogo.findIndex(pelicula => pelicula.id === idParaBorrar);
+
+    if (indice !== -1) {
+        catalogo.splice(indice, 1);
+        console.log(`Pelicula con id ${idParaBorrar} eliminada`);
+        res.json({ message: "Pelicula eliminada con éxito" });
+    } else {
+        res.status(404).json({ message: "Pelicula no encontrada" });
+    }
+});
+
+
+app.put('/peliculas/:id', (req, res) => {
+    const idParaEditar = req.params.id;
+    const datosNuevos = req.body;
+
+    const pelicula = catalogo.find(p => p.id === idParaEditar);
+
+    if (pelicula) {
+        pelicula.nombre = datosNuevos.nombre || pelicula.nombre;
+        pelicula.genero = datosNuevos.genero || pelicula.genero;
+        pelicula.imagen = datosNuevos.imagen || "/img/default.jpg";
+
+        console.log(`Pelicula editada: ${pelicula.nombre}`);
+        res.json({ message: 'Actualizada correctamente', pelicula: pelicula });
+    } else {
+        res.status(404).json({ message: 'Pelicula no encontrada' });
+    }
+});
+
 
 app.listen(PORT, (req, res) => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
